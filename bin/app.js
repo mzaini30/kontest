ce1d996a5ee2442bd065d92d808e0ee8a196e66d
mzaini30 <muhzaini30@gmail.com>
2018-02-01 19:44:48 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,18 +1,18 @@
 jQuery(function(){
 
-	// deklarasi
-
-	kunci = $('#kunci').val();
-	jawaban = $('#jawaban').val();
-	kunci_gabung = $('#kunci-gabung');
-	kunci_per_lima = $('#kunci-per-lima');
-	kunci_satuan = $('#kunci-satuan');
-	jawaban_per_lima = $('#jawaban-per-lima');
-	jawaban_satuan = $('#jawaban-satuan');
-	spss = $('#spss');
-	jawaban_anates = $('#jawaban-anates');
-
 	$('.convert').click(function(){
+
+		// deklarasi
+
+		kunci = $('#kunci').val();
+		jawaban = $('#jawaban').val();
+		kunci_gabung = $('#kunci-gabung');
+		kunci_per_lima = $('#kunci-per-lima');
+		kunci_satuan = $('#kunci-satuan');
+		jawaban_per_lima = $('#jawaban-per-lima');
+		jawaban_satuan = $('#jawaban-satuan');
+		spss = $('#spss');
+		jawaban_anates = $('#jawaban-anates');
 
 		// kunci gabung
 
